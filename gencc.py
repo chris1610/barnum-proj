2dec0756276d05fa2fe11b045977d84b556c9a79
chris.moffitt <chris.moffitt@48d779ad-112c-0410-aa88-fb0d400f1704>
2007-03-25 20:42:46 +0000

    Initial data commit.

---
+++ gencc.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+
+"""
+gencc: A simple program to generate credit card numbers that pass the MOD 10 check
+(Luhn formula).
+Usefull for testing e-commerce sites during development.
+
+Copyright 2003 Graham King
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+import random
+import sys
+import copy
+
+visaPrefixList = [ 	['4', '5', '3', '9'], 
+                    ['4', '5', '5', '6'], 
+                    ['4', '9', '1', '6'],
+                    ['4', '5', '3', '2'], 
+                    ['4', '9', '2', '9'],
+                    ['4', '0', '2', '4', '0', '0', '7', '1'],
+                    ['4', '4', '8', '6'],
+                    ['4', '7', '1', '6'],
+                    ['4'] ]
+
+mastercardPrefixList = [    ['5', '1'],
+                            ['5', '2'],
+                            ['5', '3'],
+                            ['5', '4'],
+                            ['5', '5'] ]
+
+amexPrefixList = [  ['3', '4'],
+                    ['3', '7'] ]
+
+discoverPrefixList = [ ['6', '0', '1', '1'] ]
+
+dinersPrefixList = [    ['3', '0', '0'],
+                        ['3', '0', '1'],
+                        ['3', '0', '2'],
+                        ['3', '0', '3'],
+                        ['3', '6'],
+                        ['3', '8'] ]
+
+enRoutePrefixList = [   ['2', '0', '1', '4'],
+                        ['2', '1', '4', '9'] ]
+
+jcbPrefixList16 = [   ['3', '0', '8', '8'],
+                    ['3', '0', '9', '6'],
+                    ['3', '1', '1', '2'],
+                    ['3', '1', '5', '8'],
+                    ['3', '3', '3', '7'],
+                    ['3', '5', '2', '8'] ]
+
+jcbPrefixList15 = [ ['2', '1', '0', '0'],
+                    ['1', '8', '0', '0'] ]
+
+voyagerPrefixList = [ ['8', '6', '9', '9'] ]                    
+                    
+
+"""
+'prefix' is the start of the CC number as a string, any number of digits.
+'length' is the length of the CC number to generate. Typically 13 or 16
+"""
+def completed_number(prefix, length):
+
+    ccnumber = prefix
+
+    # generate digits
+
+    while len(ccnumber) < (length - 1):
+        digit = random.choice(['0',  '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+        ccnumber.append(digit)
+
+
+    # Calculate sum 
+
+    sum = 0
+    pos = 0
+
+    reversedCCnumber = []
+    reversedCCnumber.extend(ccnumber)
+    reversedCCnumber.reverse()
+
+    while pos < length - 1:
+
+        odd = int( reversedCCnumber[pos] ) * 2
+        if odd > 9:
+            odd -= 9
+
+        sum += odd
+
+        if pos != (length - 2):
+
+            sum += int( reversedCCnumber[pos+1] )
+
+        pos += 2
+
+    # Calculate check digit
+
+    checkdigit = ((sum / 10 + 1) * 10 - sum) % 10
+
+    ccnumber.append( str(checkdigit) )
+    
+    return ''.join(ccnumber)
+
+def credit_card_number(prefixList, length, howMany):
+
+    result = []
+
+    for i in range(howMany):
+   
+        ccnumber = copy.copy( random.choice(prefixList) )
+
+        result.append( completed_number(ccnumber, length) )
+
+    return result
+
+def output(title, numbers):
+
+    result = []
+    result.append(title)
+    result.append( '-' * len(title) )
+    result.append( '\n'.join(numbers) )
+    result.append( '' )
+
+    return '\n'.join(result)
+
+#
+# Main
+#
+if __name__ == "__main__":
+
+    mastercard = credit_card_number(mastercardPrefixList, 16, 10)
+    print output("Mastercard", mastercard)
+
+    visa16 = credit_card_number(visaPrefixList, 16, 10)
+    print output("VISA 16 digit", visa16)
+
+    visa13 = credit_card_number(visaPrefixList, 13, 5)
+    print output("VISA 13 digit", visa13)
+
+    amex = credit_card_number(amexPrefixList, 15, 5)
+    print output("American Express", amex)
+
+    # Minor cards
+
+    discover = credit_card_number(discoverPrefixList, 16, 3)
+    print output("Discover", discover)
+
+    diners = credit_card_number(dinersPrefixList, 14, 3)
+    print output("Diners Club / Carte Blanche", diners)
+
+    enRoute = credit_card_number(enRoutePrefixList, 15, 3)
+    print output("enRoute", enRoute)
+
+    jcb15 = credit_card_number(jcbPrefixList15, 15, 3)
+    print output("JCB 15 digit", jcb15)
+
+    jcb16 = credit_card_number(jcbPrefixList16, 16, 3)
+    print output("JCB 16 digit", jcb16)
+
+    voyager = credit_card_number(voyagerPrefixList, 15, 3)
+    print output("Voyager", voyager)
+
---
+++ generate-data.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python2.5
+"""
+Barnum is a python-based test data generator.
+Copyright (C) 2007 Chris Moffitt
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+import cPickle as pickle
+import random
+import string
+import gencc
+import calendar
+import datetime
+import genpw
+import markov2
+
+gender_options=('Male','Female')
+company_type = ('LawFirm', 'Generic', 'Short')
+
+source_file = open("source-data.pkl",'rb')
+all_zips = pickle.load(source_file)
+state_area_codes = pickle.load(source_file)
+last_names = pickle.load(source_file)
+male_first_names = pickle.load(source_file)
+female_first_names = pickle.load(source_file)
+street_names = pickle.load(source_file)
+street_types = pickle.load(source_file)
+latin_words = pickle.load(source_file)
+email_domains = pickle.load(source_file)
+job_titles = pickle.load(source_file)
+company_names = pickle.load(source_file)
+company_types = pickle.load(source_file)
+
+source_file.close()
+
+def create_name(full_name=True, gender=None):
+    if not gender:
+        gender = random.choice(gender_options)
+    if gender == "Male":
+        first_name = random.choice(male_first_names)
+    else:
+        first_name = random.choice(female_first_names)
+    if full_name:
+        return(first_name, random.choice(last_names))
+    else:
+        return(first_name)
+
+def create_job_title():
+    return random.choice(job_titles)
+    
+def create_phone(zip_code=None):
+    if not zip_code:
+        zip_code = random.choice(all_zips.keys())
+    area_code = random.choice(state_area_codes[all_zips[zip_code][1]])
+    output = "(%s)%s-%s" % (area_code, random.randint(111,999), random.randint(1111,9999))
+    return(output)
+
+def create_street():
+    number = random.randint(1,9999)
+    name = string.capwords(random.choice(street_names))
+    street_type = string.capwords(random.choice(street_types))
+    return("%s %s %s" % (number, name, street_type))
+
+def create_city_state_zip(zip_code=None):
+    if not zip_code:
+        zip_code = random.choice(all_zips.keys())
+    return(zip_code, all_zips[zip_code][0], all_zips[zip_code][1])
+
+def create_sentence(min=4, max=15):
+    sentence = []
+    sentence.append(string.capitalize(random.choice(latin_words)))
+    for word in range(1, random.randint(min, max-1)):
+        sentence.append(random.choice(latin_words))
+    return " ".join(sentence) + "."
+    
+def create_paragraphs(num=1, min_sentences=4, max_sentences=7):
+    paragraphs = []
+    for para in range(0, num):
+        for sentence in range(1, random.randint(min_sentences, max_sentences)):
+            paragraphs.append(create_sentence()+" ")
+        paragraphs.append("\n\n")
+    return "".join(paragraphs)
+
+def create_date(numeric=True, past=False, max_years_future=10, max_years_past=10):
+    """
+    Create a random valid date
+    If past, then dates can be in the past
+    If into the futrue, then no more than max_years into the future
+    If it's not, then it can't be any older than max_years_past
+    """
+    if past:
+        start = datetime.datetime.today() - datetime.timedelta(days=max_years_past * 365)
+        #Anywhere between 1980 and today plus max_ears
+        num_days = (max_years_future * 365) + start.day
+    else:
+        start = datetime.datetime.today()
+        num_days = max_years_future * 365
+        
+    random_days = random.randint(1, num_days)
+    random_date = start + datetime.timedelta(days=random_days)    
+    return(random_date)
+
+def create_email(tld=None):
+    if not tld:
+        tld = random.choice(email_domains)
+    gender = random.choice(gender_options)
+    if gender == "Male":
+        first_name = random.choice(male_first_names)
+    else:
+        first_name = random.choice(female_first_names)
+    user = first_name + "." + random.choice(last_names)
+    domain = random.choice(latin_words) + random.choice(latin_words)
+    return ("%s@%s.%s" % (user, domain, tld))
+
+def create_company_name(biz_type=None):
+    name = []
+    if not biz_type:
+        biz_type = random.choice(company_type)
+    if biz_type == "LawFirm":
+        name.append( random.choice(last_names)+ ", " + random.choice(last_names) + " & " + 
+                     random.choice(last_names))
+        name.append('LLP')
+    else:
+        for i in range(1,random.randint(2,4)):
+            rand_name = random.choice(company_names)
+            if rand_name not in name:
+                name.append(rand_name)
+        if biz_type == 'Generic':
+            name.append(random.choice(company_types))
+        elif len(name) < 3:
+            name.append(random.choice(company_names))
+    return " ".join(name)
+
+if __name__ == "__main__":
+    first, last = create_name()
+    add = create_street()
+    zip, city, state = create_city_state_zip()
+    phone = create_phone(zip)
+    print first, last
+    print add
+    print "%s %s, %s" % (city, state,zip)
+    print phone
+    print create_sentence(), "\n"
+    print create_paragraphs(num=3)
+    mastercard = gencc.credit_card_number(gencc.mastercardPrefixList, 16, 1)
+    print "Mastercard", mastercard[0]
+    expiry = create_date(max_years_future=3)
+    print expiry.strftime("%m/%y")
+    print create_email()
+    print "Password: ", genpw.nicepass()
+    print create_company_name()
+    print create_job_title()
+
+
+
